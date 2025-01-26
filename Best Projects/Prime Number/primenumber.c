edf276b619fdea043f491fc495344b3e80497674
rduarte12 <duarterafael533@gmail.com>
2025-01-26 11:55:50 -0300

    Add files via upload

---
+++ Best Projects/Prime Number/primenumber.c
@@ -0,0 +1,70 @@
+/*
+*  Project desenvolved by git @rduart12
+*  ---------------------------------------
+*  Year: 2025
+*  Why: Learning about coding
+*
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+// Global variables
+int num;
+int divisor;
+
+/*
+* Function: Verify if a number is prime, or not.
+* The user must will input a natural number to verify,
+* else the program won't work!
+*/
+
+int main(void)
+{
+
+    //Force the user input a natural number
+    do
+    {
+        printf("For verify if a number is prime, enter a number belonging to the set of natural numbers:");
+        scanf("%d", &num);
+        printf("\n\n");
+
+        if(num <= 0)
+        {
+            printf("Invalid number\n\n");
+        }
+
+    } while(num <= 0);
+
+    //define the valor of 'divisor'
+    divisor = num - 1;
+
+
+    /*
+    * According to the mathematical definitions for prime number,
+    * determine whether it is prime or not,
+    * based on the remainders of successive divisions.
+    */
+
+    do
+    {
+        int verify = num % divisor;
+
+        if(verify != 0)
+        {
+            divisor--;
+
+        }
+        else
+        {
+            printf("The number %d is not prime\n", num);
+            divisor = 0;
+        }
+
+    }while(divisor > 1);
+
+    if(divisor == 1)
+        {
+            printf("The number %d is prime\n", num);
+        }
+}
